182aecb9f7e1b970c2cd2bffd6fc3cd43b25b45e
= <ousmanefouchimi@gmail.com>
2018-01-26 19:40:08 -0700

    Added a alternative implementation using array of fibonacci

--- src/com/practices/algos/fibonacci/Fibonacci.java
+++ src/com/practices/algos/fibonacci/Fibonacci.java
@@ -15,8 +15,20 @@ public class Fibonacci {
         return map.get(N);
     }
 
+    public int fibOptimized(int N){
+        int f[] = new int[N+1];
+        f[0] = 0;
+        f[1] = 1;
+        
+        for(int i = 2; i <= N; i++) {
+            f[i] = f[i-1] + f[i-2];
+        }
+        return f[N];
+    }
+
     public static void main(String[] args) {
         Fibonacci fib = new Fibonacci();
         System.out.println(fib.fib(9));
+        System.out.println(fib.fibOptimized(9));
     }
 }
